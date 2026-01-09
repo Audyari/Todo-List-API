24d734ddda853ac8d8834b8fd038eafe9fba4731
Audyari <audy123ari@gmail.com>
2026-01-09 17:03:17 +0700

    test Borowwing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    print!("testing")
+    let s = String::from("hello world");
+    let word = first_word(&s);
+    println!("{}", word);
+}
+
+fn first_word(s: &String) -> usize {
+    // -> usize artinya mengembalikan nilai usize
+    let bytes = s.as_bytes(); // Mengubah string menjadi array byte
+
+    println!("ini data bytes: {:?}", bytes);
+
+    for (i, &item) in bytes.iter().enumerate() {
+        // enumerate mengembalikan tuple (index, value)
+        if item == b'd' {
+            // b' ' adalah byte literal
+            return i; // Mengembalikan index pertama yang ditemukan
+        }
+    }
+
+    s.len() // Jika tidak ditemukan spasi, mengembalikan panjang string
 }
